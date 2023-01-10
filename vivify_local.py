7c317c0876b76b9ca9f8d5bcedec95424818a96d
Elena Berisha <berishaelena@gmail.com>
2023-01-10 16:02:03 -0500

    removed extra param

--- vivify_local.py
+++ vivify_local.py
@@ -2,7 +2,7 @@ import mysql.connector as mysql
 
 ## connecting to the database using 'connect()' method
 ## it takes 3 required parameters 'host', 'user', 'passwd'
-db = mysql.connect(host="localhost", user="root", passwd="password", database="sample")
+db = mysql.connect(host="localhost", user="root", passwd="password")
 
 cursor = db.cursor()
 
