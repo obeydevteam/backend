aebb492faec99d22a31b2fafedd02fa1867fa3e8
Kevin Grismore <kevin.grismore@targetsmart.com>
2023-01-09 23:09:22 -0500

    vivify_local will create a mysql database

---
+++ vivify_local.py
@@ -0,0 +1,11 @@
+import mysql.connector as mysql
+
+## connecting to the database using 'connect()' method
+## it takes 3 required parameters 'host', 'user', 'passwd'
+db = mysql.connect(host="localhost", user="root", passwd="password", database="sample")
+
+cursor = db.cursor()
+
+## 'execute()' method is used to compile a 'SQL' statement
+## below statement is used to create tha 'datacamp' database
+cursor.execute("CREATE DATABASE sample")
